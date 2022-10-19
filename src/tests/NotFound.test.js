6ef8d4226d6eec9d7131be4aa71e2f1717f906b9
Beatriz <beatriz.mediatto2@gmail.com>
2022-10-19 17:08:39 -0300

    quarto requisito concluido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testando o componente NotFound', () => {
+  test('Testa se a página contém um h2', () => {
+    renderWithRouter(<NotFound />);
+    const titleH2 = screen.getByRole('heading', { name: /Page requested not found/i, level: 2 });
+    expect(titleH2).toBeInTheDocument();
+  });
+  test('Testa se a página mostra uma imagem', () => {
+    renderWithRouter(<NotFound />);
+    const nFoundImg = screen.getByRole('img');
+    expect(nFoundImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
